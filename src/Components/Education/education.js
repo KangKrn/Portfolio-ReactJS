4c28bd5cb295fe9b02df1c181869ad10c695fb00
Kang <krnsc.kws@gmail.com>
2021-02-16 23:09:23 +0800

    Content Update

--- src/Components/Education/education.js
+++ src/Components/Education/education.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './education.scss';
 
 function Education(){
     return(
-        <div>this is education</div>
+        <Container className="educationContainer">
+            <Row>
+                This is education
+            </Row>
+        </Container>
     );
 }
 export default Education;
--- src/Components/Experience/experience.js
+++ src/Components/Experience/experience.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Table from 'react-bootstrap/Table';
 import './experience.scss';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faAngleDoubleRight} from '@fortawesome/free-solid-svg-icons'
 
+const icon = <FontAwesomeIcon icon={faAngleDoubleRight} />
 function Experience(){
     return(
         <Container className="expContainer">
@@ -11,19 +15,21 @@ function Experience(){
                 Experience
             </Row>
             <Row>
-                Frontend Web Development Intern
-                <Col>Agmo Studio</Col>
+                <Col sm={3}>Agmo Studio</Col>
+                <Col sm={9}>Frontend Web Developer | Internship</Col>
+            </Row>
+            <Row className="date">
+                Dec 2019 - Feb 2020
+            </Row>
+            <Row className="titles">
+                Angular Framework | Bootstrap | Material-UI | Typescript
             </Row>
             <Row>
-                <Col>Description</Col>
-                <Col>Agmo Studio is a software development firm offering mobile, blockchain, 
-                and web solutionos within Malaysia</Col>
-            </Row>
-            <Row>
-                <Col>Activities:</Col>
-                <Col>
-                    List
-                </Col>
+                <ul>
+                    <li><FontAwesomeIcon icon={faAngleDoubleRight}/>Learning and developing with Angular Framework</li>
+                    <li><FontAwesomeIcon icon={faAngleDoubleRight}/>Developing with Design and Backend Department</li>
+                    <li><FontAwesomeIcon icon={faAngleDoubleRight}/>Working in scrum environment</li>
+                </ul>
             </Row>
         </Container>
     );

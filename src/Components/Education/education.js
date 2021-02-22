08b72fb580d099cec1cfcc8251d633665adeea5d
Kang <krnsc.kws@gmail.com>
2021-02-22 18:52:41 +0800

    education & skills page

--- src/Components/Education/education.js
+++ src/Components/Education/education.js
@@ -3,12 +3,101 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './education.scss';
+import Timeline from '@material-ui/lab/Timeline';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
+import Paper from '@material-ui/core/Paper';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Typography from '@material-ui/core/Typography'
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 function Education(){
     return(
         <Container className="educationContainer">
             <Row>
-                This is education
+                <Col sm={3} className="title">This is Education</Col>
+                <Col sm={9} className="detailCont">
+                    <Timeline align="alternate">
+
+                        <TimelineItem>
+                            <TimelineOppositeContent>
+                                Feb 2018 - January 2021
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineDot>
+                                    <FontAwesomeIcon icon={faCircle}/>
+                                </TimelineDot>
+                                <TimelineConnector/>
+                            </TimelineSeparator>
+                            <TimelineContent>
+                                <Paper elevation={6}>
+                                    <Typography variant="h5">University of Wollongong</Typography>
+                                    <Typography variant="subtitle1">Bachelor of Information Technology</Typography>
+                                    <Typography variant="subtitle2">Majoring in e-Business & Network Design and Management</Typography>
+                                    <Typography variant="body1" className="wam">
+                                        <FontAwesomeIcon icon={faCaretRight} />
+                                        WAM (Weighted Average Marks): 68.35
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        <FontAwesomeIcon icon={faCaretRight} />
+                                        CGPA (Cumulative Grade Point Average): 3.04
+                                    </Typography>
+                                </Paper>
+                            </TimelineContent>
+                        </TimelineItem>
+
+                        <TimelineItem>
+                            <TimelineOppositeContent>
+                                February 2016 - February 2018
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineDot>
+                                    <FontAwesomeIcon icon={faCircle}/>
+                                </TimelineDot>
+                                <TimelineConnector/>
+                            </TimelineSeparator>
+                            <TimelineContent>
+                                <Paper elevation={6}>
+                                    <Typography variant="h5">INTI International College Subang Jaya</Typography>
+                                    <Typography variant="subtitle1">Diploma in Business</Typography>
+                                    <Typography variant="body1">
+                                        <FontAwesomeIcon icon={faCaretRight} />
+
+                                        CGPA (Cumulative Grade Point Average): 2.82
+                                    </Typography>
+                                </Paper>
+                            </TimelineContent>
+                        </TimelineItem>
+
+                        <TimelineItem>
+                            <TimelineOppositeContent>
+                                August 2014 - February 2016
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineDot>
+                                    <FontAwesomeIcon icon={faCircle}/>
+                                </TimelineDot>
+                                <TimelineConnector/>
+                            </TimelineSeparator>
+                            <TimelineContent>
+                                <Paper elevation={6}>
+                                    <Typography variant="h5">INTI International College Subang Jaya</Typography>
+                                    <Typography variant="subtitle1">Certificate in Business Studies</Typography>
+                                    <Typography variant="body1">
+                                        <FontAwesomeIcon icon={faCaretRight} />
+                                        CGPA (Cumulative Grade Point Average): 3.26
+                                    </Typography>
+                                </Paper>
+                            </TimelineContent>
+                        </TimelineItem>
+
+                    </Timeline>
+                </Col>
             </Row>
         </Container>
     );
--- src/Components/Skills/skills.js
+++ src/Components/Skills/skills.js
@@ -1,8 +1,78 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import './skills.scss';
+import Paper from '@material-ui/core/Paper';
+import angular from '../../Resources/angularLogo.png';
+import js from '../../Resources/javascriptLogo.png';
+import css from '../../Resources/cssLogo.png';
+import xd from '../../Resources/adobexdLogo.png';
+import scss from '../../Resources/scssLogo.png';
+import vscode from '../../Resources/vscodeLogo.png';
+import sublime from '../../Resources/sublimeLogo.png';
+import react from '../../Resources/reactLogo.png';
+import njs from '../../Resources/nodejsLogo.png';
 
 function Skills(){
     return(
-        <div>this is skills Section</div>
+        <Container className="skillsContainer">
+            <Row className="titleRow">
+                <div className="title">Profile</div>
+            </Row>
+            <Row>
+                <Container>
+                    <Row>Languages</Row>
+                    <Row>
+                        <Paper>JavaScript</Paper>
+                        <Paper>HTML5</Paper>
+                        <Paper>CSS</Paper>
+                        <Paper>Python</Paper>
+                    </Row>
+                </Container>
+            </Row>
+            <Row>
+                <Container>
+                    <Row>Frameworks</Row>
+                    <Row>
+                        <Paper>
+                            <Row><Image src={angular}/></Row>
+                            <Row>Angular</Row>
+                        </Paper>
+                        <Paper>
+                            <Row>
+                                Image
+                            </Row>
+                            <Row>
+                                React
+                            </Row>
+                        </Paper>
+                    </Row>
+                </Container>
+            </Row>
+            <Row>
+                <Container>
+                    <Row>
+                        Tools
+                    </Row>
+                    <Row>
+                        VSCode VisualStudio Sublime Word Visio AdobeXD NodeJS
+                    </Row>
+                </Container>
+            </Row>
+            <Row>
+                <Container>
+                    <Row>
+                        Pre-processors
+                    </Row>
+                    <Row>
+                        SASS
+                    </Row>
+                </Container>
+            </Row>
+            
+        </Container>
     );
 }
 export default Skills;
